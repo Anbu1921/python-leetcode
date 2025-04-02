5195d36924c6ee2ab696e507ca425a684c5d49ef
anbumurugesan19 <anbuumurugesan@gmail.com>
2025-04-02 19:34:03 +0530

    Solve Leetcode 141 (Linked List Cycle), 21 (Merge Two Sorted Lists) and 2807 (Insert Greatest common Divisors in Linked List)

---
+++ Linked List/2807. Insert Greatest Common Divisors in Linked List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = head
+        curr = head.next
+
+        while curr:
+            gcd = math.gcd(prev.val, curr.val) # G
+            g = ListNode(gcd)
+            prev.next = g
+            g.next = curr
+            prev = curr
+            curr = curr.next
+        
+        return head
+
+        # Time: O(n * G)
+        # Space: O(1)
