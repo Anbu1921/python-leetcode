9036af93ea795ee5072930a82b34d3390588f164
anbumurugesan19 <anbuumurugesan@gmail.com>
2025-04-25 21:54:59 +0530

    Solve Leetcode 215 (Kth Largest Element in an Array), 23 (Merge K Sorted Lists) and 347 (Top K Frequent Elements)

---
+++ Heap/215. Kth Largest Element in an Array.py
@@ -0,0 +1,16 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        min_heap = []
+
+        for num in nums:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, num)
+            else:
+                heapq.heappushpop(min_heap, num)
+        
+        return min_heap[0]
+
+        # Time: O(N log K)
+        # Space: O(K)
