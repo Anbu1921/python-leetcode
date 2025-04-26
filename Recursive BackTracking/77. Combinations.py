4b5613f44951509725a382932538703afedfa7bd
anbumurugesan19 <anbuumurugesan@gmail.com>
2025-04-26 22:09:46 +0530

    Solve Leetcode 46 (Permutations), 77 (Combinations) and 78 (Subsets)

---
+++ Recursive BackTracking/77. Combinations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res, sol = [], []
+
+        def backtrack(x):
+            if len(sol) == k:
+                res.append(sol[:])
+                return 
+            
+            left = x
+            still_need = k - len(sol)
+            if left > still_need:
+                backtrack(x - 1)
+            
+            sol.append(x)
+            backtrack(x - 1)
+            sol.pop()
+        
+        backtrack(n)
+        return res
+        # Time: O(N choose K)
+        # Space: O(N)
