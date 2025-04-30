8c9f9e4ec264f3a3935e4f08bdd64650b6b1c88a
anbumurugesan19 <anbuumurugesan@gmail.com>
2025-04-30 21:09:27 +0530

    Solve Leetcode 191 (Number of 1 Bits) and 504 (Base 7)

---
+++ Bit Manipulation/191. Number of 1 Bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans = 0
+
+        while n != 0:
+            ans += 1
+            n = n & (n - 1)
+        
+        return ans 
+        # Time: O(No of Ones)
+        # Space: O(1)
+        
